c187166a2a51dc11beda2c7561ea410bc3091130
Erackson Souza <souza.erackson@gmail.com>
2025-07-19 15:20:26 -0300

    refactor: remove unused ProductList and Home page components

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Header from "./components/Header"
-
-export default function Home() {
-  return (
-   <main className="bg-amber-800 h-[100vh] flex flex-col items-center justify-between p-24">
-     <h1>Home Page</h1>
-   </main>
-  );
-}
