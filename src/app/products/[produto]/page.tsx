32168437ff975229c3294c472f462ded972358c8
Erackson Souza <souza.erackson@gmail.com>
2025-07-19 11:03:54 -0300

    feat: Create product detail and products listing pages

---
+++ src/app/products/[produto]/page.tsx
@@ -0,0 +1,9 @@
+const ProdutoPage = () => {
+    return (  
+        <main>
+            <h1>Aqui vai ser o detalhamento do produto</h1>
+        </main>
+    );
+}
+ 
+export default ProdutoPage;
---
+++ src/app/products/page.tsx
@@ -0,0 +1,20 @@
+import { getAllProducts } from "../lib/api";
+import ProductCard from "../components/ProductCard";
+
+const ProductsPage = async () => {
+  const products = await getAllProducts();
+  return (
+    <main>
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-3xl font-bold mb-8">Todos os Produtos </h1>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default ProductsPage;
