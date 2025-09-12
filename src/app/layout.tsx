41da80f4610624d1cbddb2090e679f2621ff3470
Erackson Souza <souza.erackson@gmail.com>
2025-09-11 21:29:15 -0300

    fix: update API base URL and adjust product fetching methods for improved flexibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning={true}>
+      <body >
         <div className="min-h-screen flex flex-col">
           <CartProvider>
             <Header />
